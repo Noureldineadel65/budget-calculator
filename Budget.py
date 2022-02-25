9549edae24d2fc94771896d871bd99f197e196c2
Noureldine65 <noureldine.adel22@gmail.com>
2022-02-25 17:37:04 +0200

    updated app finally

--- Budget.py
+++ Budget.py
@@ -37,11 +37,11 @@ class Category:
     def get_statement(self):
         initial_deposit = sum([i["amount"] for i in list(filter(lambda x: x["amount"] > 0 and "Transfer" not in x["description"], self.ledger))])
         statement = f'''*************{self.type.title()}*************\n'''
-        statement += f'''initial deposit        {float(initial_deposit)}\n'''
+        statement += f'''initial deposit {'%6s' % float(initial_deposit)}\n'''
         for i in list(filter(lambda x: x["amount"] <= 0, self.ledger)):
-            statement += f'''{i["description"]}               {float(i["amount"])}\n'''
+            statement += f'''{i["description"]} {'%6s' % float(i["amount"])}\n'''
         for i in list(filter(lambda x: "Transfer from" in x["description"], self.ledger)):
-            statement += f'''{i["description"]}        {float(i["amount"])}\n'''
+            statement += f'''{i["description"]} {'%6s' % float(i["amount"])}\n'''
         statement += f'''Total: {self.get_balance()}'''
         return statement
 
@@ -49,31 +49,36 @@ food = Category("food")
 clothes = Category("clothes")
 food.deposit(1000, "salary")
 food.withdraw(-120, "Speeding Ticket")
-food.transfer(120, clothes)
+# food.transfer(120, clothes)
 clothes.withdraw(-90, "balmain")
 auto = Category("auto")
 auto.deposit(500)
 auto.withdraw(-200, "repair")
 
-
 def create_spend_chart(*args):
     result = {
 
     }
+
     for i in args:
-        result[i.type] = 100 - (int((i.get_balance() / sum([i["amount"] for i in list(filter(lambda x: x["amount"] > 0, i.ledger))]) * 10) % 10)) * 10
+        calc = (int((i.get_balance() / (sum([i["amount"] for i in list(filter(lambda x: x["amount"] > 0, i.ledger))]) if sum([i["amount"] for i in list(filter(lambda x: x["amount"] > 0, i.ledger))]) != 0 else 1) * 10) % 10))
+        result[i.type] = 100 - (calc * 10) if (calc * 10) != 0 else 0
+
     for (key, value) in result.items():
         result[key] = [i * 10 for i in range(value // 10 + 1)]
     for i in range(0, 11):
         turn = 100 - (i * 10)
         ok = len([n for n in result.values() if turn in n])
-
-        print('%6s' % f"  {turn}|" + " o " * ok)
+        print('%6s' % f"  {turn}|" + "o    " * ok)
     print('      ' + ("-" * round(len(args) * 3)))
     names = []
     final_name = ""
-    for i in args:
-        names.append([n for n in i.type.title()])
+    def sort_names(name):
+        return result[name.lower()][-1]
+    name_list = [i.type.title() for i in args]
+    name_list.sort(key=sort_names)
+    for i in name_list:
+        names.append([n for n in i])
     for i in range(max([len(i) for i in names])):
         for n in range(len(names)):
             try:
@@ -84,8 +89,7 @@ def create_spend_chart(*args):
 
     print('%6s' % f'{final_name}')
 
+create_spend_chart(food, auto, clothes)
 
 
 
-
-create_spend_chart(food, clothes, auto)
